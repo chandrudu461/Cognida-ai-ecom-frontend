2cf52c52601fb188755f8ea906d44b1c34a005bf
bhanuprakash026 <bhanutankasala26@gmail.com>
2023-03-09 16:43:21 +0000

    nxttrendzcartfeature4

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+  removeCartItem = id => {
+    const {cartList} = this.state
+    const updatedList = cartList.filter(eachCartItem => eachCartItem.id !== id)
+    this.setState({cartList: updatedList})
+  }
+
+  removeAllCartItems = () => {
+    this.setState({cartList: []})
+  }
+
+  decrementCartItemQuantity = id => {
+    const {cartList} = this.state
+    const productObject = cartList.find(eachCardItem => eachCardItem.id === id)
+
+    if (productObject.quantity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (eachCartItem.id === id) {
+            const updatedQuantity = eachCartItem.quantity - 1
+            return {...eachCartItem, quantity: updatedQuantity}
+          }
+          return eachCartItem
+        }),
+      }))
+    }
+  }
+
+  incrementCartItemQuantity = id => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachCartItem => {
+        if (id === eachCartItem.id) {
+          const updatedQuantity = eachCartItem.quantity + 1
+          return {...eachCartItem, quantity: updatedQuantity}
+        }
+        return eachCartItem
+      }),
+    }))
+  }
+
+  addCartItem = product => {
+    const {cartList} = this.state
+    const productObject = cartList.find(
+      eachCartItem => eachCartItem.id === product.id,
+    )
+
+    if (productObject) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (eachCartItem.id === productObject.id) {
+            const updatedQuantity = eachCartItem.quantity + product.quantity
+            return {...cartList, quantity: updatedQuantity}
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    }
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+          decrementCartItemQuantity: this.decrementCartItemQuantity,
+          incrementCartItemQuantity: this.incrementCartItemQuantity,
+          removeAllCartItems: this.removeAllCartItems,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Cart/index.js
@@ -0,0 +1,46 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+import CartSummary from '../CartSummary'
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      // TODO: Update the functionality to remove all the items in the cart
+      const onClickRemoveAll = () => {
+        removeAllCartItems()
+      }
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  className="remove-all-btn"
+                  onClick={onClickRemoveAll}
+                  type="button"
+                  data-testid="remove"
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
